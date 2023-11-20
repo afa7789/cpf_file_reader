56d8c388454bddab9eec0a831f2d5b47611cf558
Arthur Abeilice <afa7789@gmail.com>
2023-11-20 16:49:28 -0500

    Update cpf_file_reader.cpp

--- cpf_file_reader.cpp
+++ cpf_file_reader.cpp
@@ -5,15 +5,20 @@
 #include <iomanip>
 #include <ctime>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
-int main(int argc, const char **argv){
+int main(int argc, const char **argv)
+{
+
+    bool verbose = false;
 
     // hello world
     std::cout << "Hello World!" << std::endl;
 
-    if( argc < 4 || argc > 6 ){
+    if (argc < 4 || argc > 6)
+    {
         std::cerr << "Usage: cpf_file_reader <input_file_path> <output_file_path> <search_string> [<older_than>] [<younger_than>], [] means optional" << std::endl;
         return 1;
     }
@@ -29,89 +34,89 @@ int main(int argc, const char **argv){
     std::cout << "inputFilePath: " << inputFilePath << std::endl;
     std::cout << "outputFilePath: " << outputFilePath << std::endl;
 
-
     int olderThan = 0;
     int youngerThan = 200;
-    if (argc <= 6) {
+    if (argc > 4)
+    {
         // receive older than from argv
         olderThan = atoi(argv[4]);
         std::cout << "olderThan: " << olderThan << std::endl;
-        if (argc == 6) {
+        if (argc == 6)
+        {
             // receive younger than from argv
-            youngerThan =  atoi(argv[5]);
+            youngerThan = atoi(argv[5]);
             std::cout << "youngerThan: " << youngerThan << std::endl;
         }
     }
 
     std::ifstream inputFile(inputFilePath);
     std::ofstream outputFile(outputFilePath);
-    if (!inputFile) {
+    if (!inputFile)
+    {
         std::cerr << "Failed to open input file." << std::endl;
         return 1;
     }
 
-    if (!outputFile) {
+    if (!outputFile)
+    {
         std::cerr << "Failed to open output file." << std::endl;
         return 1;
     }
 
-
-    std::time_t t = std::time(nullptr);
-    std::tm* local_time = std::localtime(&t);
-    int current_year = local_time->tm_year + 1900;
+    std::time_t t;
+    std::tm *local_time;
+    int current_year;
+    if (!(olderThan == 0 && youngerThan == 200))
+    {
+        t = std::time(nullptr);
+        local_time = std::localtime(&t);
+        current_year = local_time->tm_year + 1900;
+    }
 
     std::string line;
     std::cout << "searching !" << std::endl;
-    while (std::getline(inputFile, line)) {
-        
-        // separate line by | 
+    auto start_time = chrono::high_resolution_clock::now();
+
+    while (std::getline(inputFile, line))
+    {
+
+        // separate line by |
         std::vector<std::string> seglist;
         // std::stringstream ss = "";
         std::stringstream ss(line);
         std::string token;
-        while (std::getline(ss, token, '|')) {
+        while (std::getline(ss, token, '|'))
+        {
             seglist.push_back(token);
         }
 
-        // debug seglist print it
-        // std::cout << "seglist size: " << seglist.size() << std::endl;
-        // for (int i = 0; i < seglist.size(); i++) {
-        //     std::cout << seglist[i] << std::endl;
-        // }
-
+        // the last position of the seglist array is a time in format : 05/10/1979
         std::istringstream datedSS(seglist[3]);
         std::tm tm = {};
-
         // parsing date as this format : 05/10/1979
         datedSS >> std::get_time(&tm, "%d/%m/%Y");
 
-        bool betweenAges = false;
-        // if (datedSS.fail()) {
-            // if (seglist[3] == "N/D") {
-            //     std::cout << "N/D achei" << std::endl;
-            // }else{
-                // std::cout << "Failed to parse date" << std::endl;
-                // betweenAges = false;
-            // }
-        // } else {
-         if (!datedSS.fail()) {
-            if ( olderThan == 0 && youngerThan == 200 ) {
-                std::cout << "not considering age" << std::endl;
-                // not considering age
-                betweenAges = true;
-            }else {
-                // current_year - year from input 
-                int age = current_year - ( tm.tm_year + 1900 );
-                // std::cout << "age: " << age << std::endl;
-                if (age >= olderThan && age <= youngerThan) {
+        if (!datedSS.fail() && !(olderThan == 0 && youngerThan == 200))
+        {
+            // current_year - year from input
+            int age = current_year - (tm.tm_year + 1900);
+            // std::cout << "age: " << age << std::endl;
+            if (age >= olderThan && age <= youngerThan)
+            {
+                if (verbose)
+                {
                     std::cout << "age: " << age << std::endl;
-                    betweenAges = true;
                 }
+            }
+            else
+            {
+                continue;
             }
         }
 
-        // the last position of the seglist array is a time in format : 05/10/1979
-        if ( (line.find(searchString) != std::string::npos) && betweenAges ) {
+        if ((line.find(searchString) != std::string::npos))
+        {
+
             // print found one
             std::cout << "found one" << std::endl;
             outputFile << line << std::endl;
@@ -120,6 +125,11 @@ int main(int argc, const char **argv){
 
     inputFile.close();
     outputFile.close();
-    std::cout << "closed files..." << std::endl;
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+
+    cout << "Time taken by while loop: " << duration.count() << " milliseconds" << endl;
+
     return 0;
 }
